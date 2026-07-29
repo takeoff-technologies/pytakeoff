671bed7be3262c6471d7b343d65a819667d207d4
Nicolas Bailey <nicolas.bailey.noval@gmail.com>
2026-07-29 23:23:46 +0100

    Release 0.1.3

--- src/pytakeoff/__init__.py
+++ src/pytakeoff/__init__.py
@@ -36,7 +36,7 @@ from .optimizations import Optimization2D
 from .projects import Project, ProjectsAPI
 from .transport import FlatBufferResult
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     "TakeoffClient",
